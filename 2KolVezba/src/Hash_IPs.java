d8798863be0315d2e0159c68f662208daff76263
Matej <matejst2005@gmail.com>
2026-01-06 19:43:52 +0100

    Initial commit

---
+++ 2KolVezba/src/Hash_IPs.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Hash_IPs {
+    static class Person{
+        String name;
+        int salary;
+        String ip;
+        String time;
+        String town;
+        int cost;
+        Person(){}
+        Person(String name, int salary, String ip, String time, String town, int cost){
+            this.name = name;
+            this.salary = salary;
+            this.ip = ip;
+            this.time = time;
+            this.town = town;
+            this.cost = cost;
+        }
+        void print(){
+            System.out.println(name+" with salary "+salary+" from address "+ip+" who logged in at "+time);
+        }
+    }
+    static int toMinutes(String time){
+        String[] t = time.split(":");
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        CBHT<String, ArrayList<Person>> map = new CBHT<>(n);
+        for (int i = 0; i < n; i++) {
+            String[] parts = sc.nextLine().split("\\s+");
+            String[] ip = parts[3].split("\\.");
+            String ipaddr = ip[0]+"."+ip[1]+"."+ip[2];
+            Person p = new Person(parts[0]+" "+parts[1], Integer.parseInt(parts[2]), parts[3], parts[4], parts[5], Integer.parseInt(parts[6]));
+            SLLNode<MapEntry<String, ArrayList<Person>>> node = map.search(ipaddr);
+            if (node == null){
+                ArrayList<Person> list = new ArrayList<>();
+                list.add(p);
+                map.insert(ipaddr, list);
+            }
+            else{
+                node.element.value.add(p);
+            }
+        }
+        int m = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < m; i++) {
+            String[] parts = sc.nextLine().split("\\s+");
+            String[] ip = parts[3].split("\\.");
+            String ipaddr = ip[0] + "." + ip[1] + "." + ip[2];
+            System.out.println("IP network: " + ipaddr + " has the following number of users:");
+            SLLNode<MapEntry<String, ArrayList<Person>>> node = map.search(ipaddr);
+            ArrayList<Person> list = node.element.value;
+            int pocetok = 12*60;
+            int count = 0;
+            Person earliest = null;
+            int mintime = Integer.MAX_VALUE;
+            for (Person p : list){
+                int t = toMinutes(p.time);
+                if (t >= pocetok){
+                    if (t < mintime) {
+                        earliest = p;
+                        mintime = t;
+                    }
+                    count++;
+                }
+            }
+            System.out.println(count);
+            System.out.println("The user who logged on earliest after noon from that network is:");
+            earliest.print();
+            System.out.println();
+        }
+    }
+}
